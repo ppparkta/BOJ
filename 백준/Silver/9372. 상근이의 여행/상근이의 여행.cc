c79e0a15b033baae0a6e74c6d0fa3fe156e0e264
sooyang <86940801+ppparkta@users.noreply.github.com>
2022-07-10 18:46:07 +0900

    [Silver IV] Title: 상근이의 여행, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,19 @@
+#include	<iostream>
+using namespace std;
+
+int main() {
+    int t, n, m;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n >> m;
+        for (int j = 0; j < m; j++)
+        {
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << n - 1 << endl;
+    }
+    
+    return 0;
+}
