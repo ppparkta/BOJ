e10355419391101a6161b591bb64835f9d5203d0
sooyang <86940801+ppparkta@users.noreply.github.com>
2022-07-27 00:08:58 +0900

    [Silver II] Title: 최소 힙, Time: 20 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,32 @@
+#include	<iostream>
+#include	<queue>
+#include	<functional>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> q;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		if (x != 0)
+			q.push(x);
+		else
+		{
+			if (q.empty())
+				cout << 0 << '\n';
+			else
+			{
+				cout << q.top() << '\n';
+				q.pop();
+			}
+		}
+	}
+	return 0;
+}
