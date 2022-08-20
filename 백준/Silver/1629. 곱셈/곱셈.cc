a5746e18206a324722e91d86eda35eb92f6b56fc
sooyang <86940801+ppparkta@users.noreply.github.com>
2022-08-20 17:32:30 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,22 @@
+#include    <iostream>
+using namespace std;
+
+long long a,b,c,d;
+
+long long power(long long b)
+{
+    if(b==0)
+        return 1;
+    if(b==1)
+        return (a%c);
+    d=power(b/2)%c;
+    if(b%2==0)
+        return (d*d%c);
+    return (d*d%c*a%c);
+}
+
+int main()
+{
+    cin>>a>>b>>c;
+    cout<<power(b);
+}
