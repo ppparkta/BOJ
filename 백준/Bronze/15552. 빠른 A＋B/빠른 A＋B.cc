d6e856b7ab27e3c5afb9fd613617cb5b38efad67
sooyang <86940801+ppparkta@users.noreply.github.com>
2022-09-02 04:23:15 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include    <iostream>
+using namespace std;
+
+int t, a, b;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        cout<<a+b<<'\n';
+    }
+    return (0);
+}
