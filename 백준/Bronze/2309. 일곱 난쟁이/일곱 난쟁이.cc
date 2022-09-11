fc8013aec390c272930027a3285d1fe6c5675224
sooyang <86940801+ppparkta@users.noreply.github.com>
2022-09-12 02:08:02 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -2,68 +2,31 @@
 #include	<algorithm>
 using namespace std;
 
-int add, arr[9], ans[7];
-bool swc;
+int add, arr[9];
 
 int main()
 {
-	swc = false;
 	for (int i = 0; i < 9; i++)
 	{
 		cin >> arr[i];
+		add += arr[i];
 	}
 	sort(arr, arr + 9);
 	for (int i = 0; i < 9; i++)
 	{
-		ans[0] = arr[i];
 		for (int j = i + 1; j < 9; j++)
 		{
-			ans[1] = arr[j];
-			for (int k = j + 1; k < 9; k++)
+			if (add - (arr[i] + arr[j]) == 100)
 			{
-				ans[2] = arr[k];
-				for (int l = k + 1; l < 9; l++)
+				for (int k = 0; k < 9; k++)
 				{
-					ans[3] = arr[l];
-					for (int n = l + 1; n < 9; n++)
+					if (k != i && k != j)
 					{
-						ans[4] = arr[n];
-						for (int m = n + 1; m < 9; m++)
-						{
-							ans[5] = arr[m];
-							for (int o = m + 1; o < 9; o++)
-							{
-								ans[6] = arr[o];
-								add = arr[i] + arr[j] + arr[k] + arr[l] + arr[n] + arr[m] + arr[o];
-								if (add == 100)
-								{
-									for (int nu = 0; nu < 7; nu++)
-									{
-										cout << ans[nu] << '\n';
-									}
-									swc = true;
-									break;
-								}
-								if (swc == true)
-									break;
-							}
-							if (swc == true)
-								break;
-						}
-						if (swc == true)
-							break;
+						cout << arr[k] << '\n';
 					}
-					if (swc == true)
-						break;
 				}
-				if (swc == true)
-					break;
+				return 0;
 			}
-			if (swc == true)
-				break;
 		}
-		if (swc == true)
-			break;
 	}
-	return(0);
 }
