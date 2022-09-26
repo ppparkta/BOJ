c6d6b754f9e4c774ab08dd207bc278d20f4b031d
sooyang <86940801+ppparkta@users.noreply.github.com>
2022-09-27 00:27:12 +0900

    [Silver III] Title: 다음 순열, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -0,0 +1,23 @@
+#include	<iostream>
+#include	<algorithm>
+using namespace std;
+int n;
+int arr[10001];
+bool check;
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	check=next_permutation(arr, arr + n);
+	if (check == false)
+	{
+		cout << -1 << endl;
+		return 0;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
