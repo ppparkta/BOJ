8c60127a17de21f6a6c55dd54d58b30cf0f4be0a
sooyang <86940801+ppparkta@users.noreply.github.com>
2025-07-22 00:12:50 +0900

    [Bronze I] Title: 세로읽기, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+// https://www.acmicpc.net/problem/10798
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] arr = new char[5][15];
+        int max = 0;
+
+        for (int i = 0; i<5; i++) {
+            String s = br.readLine();
+            max = Math.max(max, s.length());
+            for (int j = 0; j < s.length(); j++) {
+                arr[i][j] = s.charAt(j);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] == '\0') continue; 
+                sb.append(arr[j][i]);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
