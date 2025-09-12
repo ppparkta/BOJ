0871879a03be610dd48399647da5688e0cb38b90
sooyang <86940801+ppparkta@users.noreply.github.com>
2025-09-12 11:55:49 +0900

    [Silver II] Title: DFS와 BFS, Time: 288 ms, Memory: 21252 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,78 +1,79 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+//  DFS와 BFS
 // https://www.acmicpc.net/problem/1260
-public class Main {
-    static int Edge_arr[][];
-    static boolean Visited_arr[];
-    static int N;
-    static int M;
-    static int V;
-    static int count;
-    static Queue<Integer> que = new ArrayDeque<>();
+class Main {
+    static int n;
+    static int m;
+    static int k;
+    static List<List<Integer>> arr;
+    static boolean visitDfs[];
+    static boolean visitBfs[];
 
-    public static void BFS(int start) {
-        que.offer(start);
-        Visited_arr[start] = true;
-        System.out.print(start + " ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        while (!que.isEmpty()) {
-            start = que.poll();
-
-            for (int i = 1; i <= N; i++) {
-
-                if (Edge_arr[start][i] == 1 && Visited_arr[i] == false) {
-                    que.offer(i);
-                    Visited_arr[i] = true;
-                    System.out.print(i + " ");
-                }
-            }
+        visitDfs = new boolean[n + 1];
+        visitBfs = new boolean[n + 1];
+        arr = new ArrayList<>(n + 1);
+        for (int i = 0; i <= n; i++) {
+            arr.add(new ArrayList<>());
         }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr.get(a).add(b);
+            arr.get(b).add(a);
+        }
+        for (int i = 0; i <= n; i++) {
+            Collections.sort(arr.get(i));
+        }
+        System.out.print(k + " ");
+        visitDfs[k] = true;
+        dfs(k, 0);
+        bfs();
     }
 
-    public static void DFS(int start) {
-        Visited_arr[start] = true;
-        System.out.print(start + " ");
-
-        if (count == N) {
+    private static void dfs(int node, int depth) {
+        if (depth == n) {
             return;
         }
-        count++;
-
-        for (int i = 1; i <= N; i++) {
-            if (Edge_arr[start][i] == 1 && Visited_arr[i] == false) {
-                DFS(i);
+        for (int i = 0; i < arr.get(node).size(); i++) {
+            if (visitDfs[arr.get(node).get(i)] == true) {
+                continue;
             }
+            System.out.print(arr.get(node).get(i) + " ");
+            visitDfs[arr.get(node).get(i)] = true;
+            dfs(arr.get(node).get(i), depth + 1);
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
-
-        Edge_arr = new int[1001][1001];
-        Visited_arr = new boolean[1001];
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            Edge_arr[x][y] = Edge_arr[y][x] = 1;
+    private static void bfs() {
+        Deque<Integer> queue = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        int node = k;
+        sb.append("\n" + node + " ");
+        queue.push(node);
+        visitBfs[node] = true;
+        while (!queue.isEmpty()) {
+            node = queue.pop();
+            for (int i = 0; i < arr.get(node).size(); i++) {
+                if (visitBfs[arr.get(node).get(i)] == true) {
+                    continue;
+                }
+                queue.offer(arr.get(node).get(i));
+                sb.append(arr.get(node).get(i) + " ");
+                visitBfs[arr.get(node).get(i)] = true;
+            }
         }
-
-        DFS(V);
-        System.out.println();
-
-        Visited_arr = new boolean[1001];
-        BFS(V);
+        System.out.println(sb.toString());
     }
 }
