1101faedf5c08f07a3b73750d1d9f6bb327bb320
sooyang <86940801+ppparkta@users.noreply.github.com>
2025-10-24 00:21:26 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14868 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,76 +6,64 @@ import java.io.*;
 
 class Main {
 
-    public static int dx[] = {0, 0, -1, 1};
+    public static int dx[] = {0, 0, 1, -1};
     public static int dy[] = {1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // n개의 줄
+
+        //y
         int n = Integer.parseInt(st.nextToken());
-        // m개의 정수
+        // x
         int m = Integer.parseInt(st.nextToken());
 
         int arr[][] = new int[n][m];
 
-        // 그래프 채우기
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
             for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
+                arr[i][j] = s.charAt(j) - '0';
             }
         }
 
-        // 반복하며 이동하기
+        Deque<int[]> dq = new ArrayDeque<>();
         boolean visited[][] = new boolean[n][m];
         int dist[][] = new int[n][m];
-        Deque<Pair> deque = new ArrayDeque<>();
-        // x, y 순서로 삽입
-        deque.offer(new Pair(0, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        dq.offer(new int[]{0, 0});
         visited[0][0] = true;
         dist[0][0] = 1;
-        while (!deque.isEmpty()) {
-            Pair removed = deque.poll();
-            int x = removed.getX();
-            int y = removed.getY();
-            if (x == m - 1 && y == n - 1) {
+
+        while (!dq.isEmpty()) {
+            int[] poll = dq.poll();
+            int y = poll[0];
+            int x = poll[1];
+//            System.out.println("y: " + y + ", x: " + x);
+            if (y == n - 1 && x == m - 1) {
                 System.out.println(dist[y][x]);
                 break;
             }
             for (int i = 0; i < 4; i++) {
-                int sx = dx[i];
-                int sy = dy[i];
-                int nx = sx + x;
-                int ny = sy + y;
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
                     continue;
                 }
-                if (arr[ny][nx] == 0 || visited[ny][nx] == true) {
+                if (visited[ny][nx] == true || arr[ny][nx] == 0) {
                     continue;
                 }
                 visited[ny][nx] = true;
-                dist[ny][nx] = dist[y][x] + 1;
-                deque.offer(new Pair(nx, ny));
+                dist[ny][nx] = Math.min(dist[ny][nx], dist[y][x] + 1);
+                dq.offer(new int[]{ny, nx});
             }
         }
-    }
 
-    static class Pair {
-        public int x;
-        public int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
     }
 }
