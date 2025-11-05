285b7a741ddc18b283c7686bd3a2bf0958e1f1df
sooyang <86940801+ppparkta@users.noreply.github.com>
2025-11-06 03:51:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.44 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 98 MB, 시간: 95.42 ms
+메모리: 96.7 MB, 시간: 91.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 13일 10:22:07
+2025년 11월 06일 03:51:27
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,27 +2,21 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
-        Map<String, Integer> part = new HashMap<>();
-        
-        for(int i=0;i<participant.length; i++){
-            if (!part.containsKey(participant[i])) {
-                part.put(participant[i], 1);
-            } else {
-                 part.put(participant[i], part.get(participant[i])+ 1);
+        Map<String, Integer> s = new HashMap<>();
+        for(String p : participant) {
+            s.put(p, s.getOrDefault(p, 0) + 1);
+        }
+        for(String c : completion) {
+            if (s.containsKey(c)) {
+                s.put(c, s.get(c) - 1);
             }
         }
-        
-        for(int i=0;i<completion.length;i++){
-            part.put(completion[i], part.get(completion[i]) - 1);
+        for(String key : s.keySet()) {
+            if (s.get(key) != 0){
+                return key;
+            }
         }
         String answer = "";
-        for(int i=0;i<part.size();i++){
-            if (part.get(participant[i]) > 0){
-                answer = participant[i];
-            }
-        }
-        
         return answer;
     }
 }
