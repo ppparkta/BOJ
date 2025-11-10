9eeb9db874ddff65cf449f6b319fca0d89d84b16
sooyang <86940801+ppparkta@users.noreply.github.com>
2025-11-10 18:17:34 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 116.21 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -9,7 +9,6 @@ class Solution {
     public static int[][] oil;
     public static int r, c;
     
-    // 시간초과 문제 -> bfs 단순탐색말거 캐싱
     public int solution(int[][] land) {
         arr = land;
         r = land.length;
@@ -17,59 +16,59 @@ class Solution {
         visited = new boolean[r][c];
         oil = new int[r][c];
 
-        // 오일 덩어리 크기, 덩어리마다 포함된 좌표 
         Map<Integer, Integer> oilSize = new HashMap<>();
-        Map<Integer, Set<Integer>> oilCols = new HashMap<>();
         int id = 1;
         
-        // 덩어리별로 다 돌ㄹ아버려
-        // 다 도는거? dfs임
         for (int y = 0; y < r; y++) {
             for (int x = 0; x < c; x++) {
                 if (land[y][x] == 1 && !visited[y][x]) {
                     Set<Integer> cols = new HashSet<>();
-                    int size = dfs(y, x, id, cols);
+                    int size = bfs(y, x, id, cols);
                     oilSize.put(id, size);
-                    oilCols.put(id, cols);
                     id++;
                 }
             }
         }
         
-        // 덩어리 걸치는 구간 화긴
-        // gkTlqkf
         int answer = 0;
         for (int x = 0; x < c; x++) {
-            Set<Integer> seen = new HashSet<>();
-            int total = 0;
+            Set<Integer> s = new HashSet<>();
+            int cnt = 0;
             for (int y = 0; y < r; y++) {
                 int curId = oil[y][x];
-                if (curId != 0 && !seen.contains(curId)) {
-                    seen.add(curId);
-                    total += oilSize.get(curId);
+                if (curId != 0 && !s.contains(curId)) {
+                    s.add(curId);
+                    cnt += oilSize.get(curId);
                 }
             }
-            answer = Math.max(answer, total);
+            answer = Math.max(answer, cnt);
         }
 
         return answer;
     }
     
-    private int dfs(int y, int x, int id, Set<Integer> cols) {
-        visited[y][x] = true;
-        oil[y][x] = id;
-        cols.add(x);
-        int cnt = 1;
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= r || nx >= c) {
-                continue;
+    private int bfs(int sy, int sx, int id, Set<Integer> cols) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{sy, sx});
+        visited[sy][sx] = true;
+        oil[sy][sx] = id;
+        cols.add(sx);
+
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            cnt++;
+            int y = cur[0], x = cur[1];
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if (ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+                if (visited[ny][nx] || arr[ny][nx] == 0) continue;
+                visited[ny][nx] = true;
+                oil[ny][nx] = id;
+                cols.add(nx);
+                q.offer(new int[]{ny, nx});
             }
-            if (visited[ny][nx] || arr[ny][nx] == 0) {
-                continue;
-            }
-            cnt += dfs(ny, nx, id, cols);
         }
         return cnt;
     }
